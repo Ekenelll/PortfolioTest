2363f0ea966f9ba230079b3db50fd75a0ecb381e
Ekenelll <ekenelpierce3227@gmail.com>
2024-02-03 15:44:24 +0800

    font styles

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var video = document.getElementById('mainVideo');
+    var allVideos = document.querySelectorAll('MainVideos');
     var titleOverlay = document.getElementById('videoTitle');
 
-    video.addEventListener('play', function () {
-        titleOverlay.style.display = 'none';
-    });
+    function handleVideoEvents(video) {
+        video.addEventListener('play', function () {
+            titleOverlay.style.display = 'none';
+        });
 
-    video.addEventListener('pause', function () {
-        titleOverlay.style.display = 'block';
-    });
+        video.addEventListener('pause', function () {
+            titleOverlay.style.display = 'block';
+        });
 
-    // Add an additional event listener for 'ended' to show the title when the video ends
-    video.addEventListener('ended', function () {
-        titleOverlay.style.display = 'block';
+        // Add an additional event listener for 'ended' to show the title when the video ends
+        video.addEventListener('ended', function () {
+            titleOverlay.style.display = 'block';
+        });
+    }
+
+    allVideos.forEach(function (video) {
+        handleVideoEvents(video);
     });
 });
